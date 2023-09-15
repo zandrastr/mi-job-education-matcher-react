ee5d281aaa6d64403d729b962a60b2b85af20cd5
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-09-15 17:10:53 +0200

    Styled components

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,6 +1,19 @@
+import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen"
+import { ButtonTest } from "../style/Buttons"
+import { DigiButton } from "@digi/arbetsformedlingen-react"
+
+
 export const Home = () => {
     return (<>
 
-    <h1>123</h1>
+    <ButtonTest>Styled knapp</ButtonTest>
+
+
+    <DigiButton
+	afSize={ButtonSize.MEDIUM}
+	afVariation={ButtonVariation.SECONDARY}
+	afFullWidth={false}>
+	Af knapp
+    </DigiButton>
     </>)
 }
