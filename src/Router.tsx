b3dd794bc4200b37e9296c761e4d2f528e3df2e7
Malinaahlin <malinahlin1@gmail.com>
2023-09-18 14:06:11 +0200

    Error handling

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./Components/Layout";
 import { Home } from "./Components/Home";
 import { TestForm } from "./Components/TestForm";
+import { Error } from './pages/Error';
 
 export const router = createBrowserRouter([
     {
         path:"/",
         element:<Layout></Layout>,
+        errorElement: <Error></Error>,
         children: [
             {
                 path: "/",
---
+++ src/pages/Error.tsx
@@ -0,0 +1,7 @@
+export const Error = () => {
+    return (
+        <>
+            <h1>Sidan hittades inte!</h1>
+        </>
+    );
+};
