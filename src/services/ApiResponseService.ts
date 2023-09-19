2a9b00c6ce89157a3af972ef83a1681392671f29
Sandra S <zandrastr@hotmail.com>
2023-09-19 13:50:00 +0200

    update api service

--- src/services/ApiResponseService.ts
+++ src/services/ApiResponseService.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
-import { IRelatedOccupationsApiResult } from "../models/RelatedOccupationsInterface";
+import { IOccupation, IRelatedOccupationsApiResult } from "../models/RelatedOccupationsInterface";
 
-export const getRelatedOccupationsFromApi = async () => {
-    const testInput = "developer"; //Input är hårdkodad här för att testa API-anropet. 
+export const getRelatedOccupationsFromApi = async (userInput: string): Promise<IOccupation[] | undefined> => {
 
     try {
       const result: IRelatedOccupationsApiResult = await axios.post(
-        `https://jobed-connect-api.jobtechdev.se/v1/occupations/match-by-text?input_text=${testInput}&input_headline=${testInput}&limit=10&offset=0&include_metadata=false`
+        `https://jobed-connect-api.jobtechdev.se/v1/occupations/match-by-text?input_text=${userInput}&input_headline=${userInput}&limit=10&offset=0&include_metadata=false`
       );
       console.log("API result:", result.data);
       console.log("All related occupations:", result.data.related_occupations);
       console.log("Show ONE occupation result:", result.data.related_occupations[0].occupation_label);
+      return result.data.related_occupations;
     } catch (error) {
       console.log("Error getting data from API:", error);
     }
