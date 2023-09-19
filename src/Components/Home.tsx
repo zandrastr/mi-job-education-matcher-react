4127dc9b55e9d7aaf92734c3cb7fc33cf2827336
Sandra S <zandrastr@hotmail.com>
2023-09-19 13:51:17 +0200

    add handle click function to mocked user input

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,12 +2,30 @@ import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen";
 import { ButtonTest } from "../style/Buttons";
 import { DigiButton } from "@digi/arbetsformedlingen-react";
 import { getRelatedOccupationsFromApi } from "../services/ApiResponseService";
+import { useState } from "react";
+import { IOccupation } from "../models/RelatedOccupationsInterface";
+import { RelatedOccupations } from "./RelatedOccupations";
+
+const testInput: string = "developer"; //Input är hårdkodad här för att testa API-anropet.
 
 export const Home = () => {
+  const [relatedOccupations, setRelatedOccupations] = useState<IOccupation[]>([]);
+
+  const handleClick = async () => {
+    const data = await getRelatedOccupationsFromApi(testInput);
+    if (data) {
+      setRelatedOccupations(data);
+    }
+  };
+
   return (
     <>
-      {/* La in onClick-funktion med API-anropet här för att testa. */}
-      <ButtonTest onClick={getRelatedOccupationsFromApi}>Styled knapp</ButtonTest>
+      {/* La in on Click-funktion med API-anropet här för att testa. */}
+      <button onClick={handleClick}>Send mocked user input</button>
+      {relatedOccupations.length > 0 && <RelatedOccupations props={relatedOccupations} />}
+
+      <ButtonTest>Styled knapp</ButtonTest>
+
       <DigiButton
         afSize={ButtonSize.MEDIUM}
         afVariation={ButtonVariation.SECONDARY}
