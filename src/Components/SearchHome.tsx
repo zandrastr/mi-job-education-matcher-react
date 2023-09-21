0b293c7274d4a19f2e94ace07fe6f00868b3c8f9
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-09-22 00:07:28 +0200

    Link i utskrift
    Co-Authored-By: Malin Åhlin <113439335+Malinaahlin@users.noreply.github.com>

--- src/Components/SearchHome.tsx
+++ src/Components/SearchHome.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "../style/TestForm.css";
-import { DigiButton, DigiFormInput } from "@digi/arbetsformedlingen-react";
-import { ButtonSize, ButtonVariation, FormInputMode, FormInputType, FormInputValidation, FormInputVariation } from "@digi/arbetsformedlingen";
+import { DigiButton, DigiFormInput, DigiLink } from "@digi/arbetsformedlingen-react";
+import { ButtonSize, ButtonVariation, FormInputMode, FormInputType, FormInputValidation, FormInputVariation, LinkVariation } from "@digi/arbetsformedlingen";
 import { IOccupation } from "../models/RelatedOccupationsInterface";
 import { getRelatedOccupationsFromApi } from "../services/ApiResponseService";
+import { Link } from "react-router-dom";
 
 const generateSubstrings = (input: string): string[] => {
   const substrings: string[] = [];
@@ -67,10 +68,17 @@ export const SearchHome = () => {
         <>
           <h2>Relevanta yrken:</h2>
           <ul>
-            {relatedOccupations.map((occupation) => (
-              <li key={occupation.id}>{occupation.occupation_label}</li>
-            ))}
-          </ul>
+          {relatedOccupations.map((occupation) => (
+            <li key={occupation.id}>
+              <DigiLink afHref="/" afVariation={LinkVariation.LARGE}>
+                <Link to={`/${occupation.occupation_label}`}>
+                  {occupation.occupation_label}
+                </Link>
+              </DigiLink>
+            </li>
+          ))}
+        </ul>
+
         </>
       
     </div>
