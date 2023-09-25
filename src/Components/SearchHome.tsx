e49496ea78b64c43034a5296b8a595f27e0c89da
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-09-25 12:42:08 +0200

    Felmeddelande ifall sökningen inte gav några resultat

--- src/Components/SearchHome.tsx
+++ src/Components/SearchHome.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "../style/TestForm.css";
 
-import { DigiFormInputSearch } from "@digi/arbetsformedlingen-react";
-import { FormInputSearchVariation, FormInputType } from "@digi/arbetsformedlingen";
+import { DigiFormInputSearch, DigiList } from "@digi/arbetsformedlingen-react";
+import { FormInputSearchVariation, FormInputType, ListType } from "@digi/arbetsformedlingen";
 
 import { IOccupation } from "../models/RelatedOccupationsInterface";
 import { ICompetency } from "../models/CompentenciesInterface";
@@ -16,45 +16,72 @@ export const SearchHome = () => {
   const [relatedOccupations, setRelatedOccupations] = useState<IOccupation[]>([]);
   const [competencies, setCompetencies] = useState<ICompetency[]>([]);
   const [selectedOccupationId, setSelectedOccupationId] = useState<string | null>(null);
-
+  const [hasSearched, setHasSearched] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const handleChange = (name: string, value: string) => {
-    if (name === "titelInput") {
-      setTitelInput(value);
-      handleSubmit(value);
-    }
+      if (name === "titelInput") {
+          setTitelInput(value);
+          // Clearing the previous data before a new search
+          setRelatedOccupations([]);
+          setCompetencies([]);
+          setSelectedOccupationId(null);
+          handleSubmit(value);
+      }
   };
 
   const handleSubmit = (inputValue: string) => {
     handleSearchSubmit(inputValue, setRelatedOccupations);
- };
-
- 
-const handleClickToGetCompetencies = (occupationId: string) => {
-  handleCompetencyClick(occupationId, selectedOccupationId, setSelectedOccupationId, setCompetencies);
+    setHasSearched(true);
+    setTimeout(() => {
+        if (relatedOccupations.length === 0) {
+            setShowError(true);
+        }
+    }, 2000);
 };
 
-return (
-  <div>
-      <DigiFormInputSearch
-          afLabel="Hitta relaterade yrken"
-          afVariation={FormInputSearchVariation.LARGE}
-          afType={FormInputType.SEARCH}	
-          value={titelInput}
-          afButtonText="Sök"
-          onAfOnChange={(e) =>
-              handleChange("titelInput", (e.target as unknown as HTMLInputElement).value)
-          }
-      />
-        
-      <h2>Relevanta yrken:</h2>
-      <OccupationMenu 
-                occupations={relatedOccupations} 
-                competencies={competencies} 
-                selectedOccupationId={selectedOccupationId} 
-                onCompetencyClick={handleClickToGetCompetencies}
-            />
-  </div>
-);
+  const handleClickToGetCompetencies = (occupationId: string) => {
+      handleCompetencyClick(occupationId, selectedOccupationId, setSelectedOccupationId, setCompetencies);
+  };
 
+  return (
+      <div>
+          <DigiFormInputSearch
+              afLabel="Hitta relaterade yrken"
+              afVariation={FormInputSearchVariation.LARGE}
+              afType={FormInputType.SEARCH}	
+              value={titelInput}
+              afButtonText="Sök"
+              onAfOnChange={(e) =>
+                  handleChange("titelInput", (e.target as unknown as HTMLInputElement).value)
+              }
+          />
+          
+          
+          {hasSearched && showError && relatedOccupations.length === 0 ? (
+              <div className="error-message">
+                <h3>Inga yrken hittades vid din sökning. Tips för att förbättra din sökning</h3>
+                            <DigiList
+            afListType={ListType.BULLET}
+          >
+            
+          <li>Kontrollera din stavning</li>
+          <li>Byt ut eller ta bort ett sökord</li>
+              </DigiList>
+              </div>
+          ) : (
+            relatedOccupations.length > 0 && (
+                <>
+                    <h2>Relevanta yrken:</h2>
+                    <OccupationMenu 
+                        occupations={relatedOccupations} 
+                        competencies={competencies} 
+                        selectedOccupationId={selectedOccupationId} 
+                        onCompetencyClick={handleClickToGetCompetencies}
+                    />
+                </>
+            )
+        )}
+    </div>
+  );
 };
